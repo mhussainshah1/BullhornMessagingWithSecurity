7931358273cb72a0767080d7e74310b744ff3a59
mhussainshah1 <muhammadshah.14@sunymaritime.edu>
2019-03-25 02:19:10 -0400

    commit

--- src/main/java/com/example/demo/business/util/PasswordConstraintValidator.java
+++ src/main/java/com/example/demo/business/util/PasswordConstraintValidator.java
@@ -21,7 +21,7 @@ public class PasswordConstraintValidator implements ConstraintValidator<ValidPas
     @Override
     public void initialize(ValidPassword constraintAnnotation) {
         try {
-            String invalidPasswordList = this.getClass().getResource("/invalid-password-list.txt").getFile();
+            String invalidPasswordList = this.getClass().getResource("invalid-password-list.txt").getFile();
             dictionaryRule = new DictionaryRule(
                     new WordListDictionary(WordLists.createFromReader(
                             // Reader around the word list file
