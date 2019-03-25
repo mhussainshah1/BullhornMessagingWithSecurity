fc118e58a4b7613ba6ec261180223121c1ae6538
mhussainshah1 <muhammadshah.14@sunymaritime.edu>
2019-03-25 02:34:20 -0400

    commit

--- src/main/java/com/example/demo/web/config/SecurityConfiguration.java
+++ src/main/java/com/example/demo/web/config/SecurityConfiguration.java
@@ -36,7 +36,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception{
         http
                 .authorizeRequests()
-                .antMatchers("/","/termsandconditions", "/css/**","/js/**","/h2-console/**", "/register", "/about").permitAll()
+                .antMatchers("/","/termsandconditions",
+                        "/css/**","/js/**","/h2-console/**", "/register",
+                        "/detail/{id}","/about").permitAll()
 // , "/detail/** "
 //                .access("hasAnyAuthority('USER','ADMIN')")
 //                .antMatchers("/admin").access("hasAuthority('ADMIN')")
