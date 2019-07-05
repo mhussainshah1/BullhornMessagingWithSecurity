b40f8584df7991fe9abb95dbdacd8f60450baf89
mhussainshah1 <muhammadshah.14@sunymaritime.edu>
2019-07-05 19:14:43 -0400

    follow controller

--- src/main/java/com/example/demo/web/controller/HomeController.java
+++ src/main/java/com/example/demo/web/controller/HomeController.java
@@ -150,40 +150,6 @@ public class HomeController {
         return "profile";
     }
 
-    @RequestMapping("/followers")
-    public String getFollowers(Model model) {
-        model.addAttribute("message", "My Followers");
-        model.addAttribute("md5Util", new MD5Util());
-        model.addAttribute("users", userRepository.findAllByFollowings(userService.getUser()));
-        return "peoplelist";
-    }
-
-    @RequestMapping("/following")
-    public String getFollowing(Model model) {
-        model.addAttribute("message", "People I`m Following");
-        model.addAttribute("md5Util", new MD5Util());
-        model.addAttribute("users", userRepository.findAllByFollowers(userService.getUser()));
-        return "peoplelist";
-    }
-
-    @RequestMapping("/follow/{id}")
-    public String follow(@PathVariable("id") long id, Model model) {
-        User follow = userRepository.findById(id).get();
-        User myuser = userService.getUser();
-        myuser.addFollowing(follow);
-        userRepository.save(myuser);
-        return "redirect:/";
-    }
-
-    @RequestMapping("/unfollow/{id}")
-    public String unfollow(@PathVariable("id") long id, Model model) {
-        User follow = userRepository.findById(id).get();
-        User myuser = userService.getUser();
-        myuser.removeFollowing(follow);
-        userRepository.save(myuser);
-        return "redirect:/";
-    }
-
     //AUXILLARY FUNCTION!!!
     //Use the below code INSIDE METHOD to pass user into view
     @RequestMapping("/secure")
